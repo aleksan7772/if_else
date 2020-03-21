85ea1a9e28d83f5f2d1eb021c19fcb75ddd22677
aleksan7772 <61800484+aleksan7772@users.noreply.github.com>
2020-03-22 00:44:56 +0200

    Even_NotEven

---
+++ Main/src/Faktorial.java
@@ -0,0 +1,31 @@
+public class Faktorial {
+    // Evaluate n!
+
+    public static long factorial( int n )
+
+    {
+
+        if( n <= 1 )     // base case
+
+            return 1;
+
+        else
+
+            return n * factorial( n - 1 );
+
+    }
+
+
+
+    // Simple test program
+
+    public static void main( String [ ] args )
+
+    {
+
+        for( int i = 1; i <= 10; i++ )
+
+            System.out.println( factorial( i ) );
+
+    }
+}
