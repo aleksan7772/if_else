2263a7ca4d47c9c0ec4f375ff2774fdb1f16403b
aleksan7772 <61800484+aleksan7772@users.noreply.github.com>
2020-03-22 00:13:10 +0200

    Even_NotEven

---
+++ Main/src/Even_NotEven.java
@@ -0,0 +1,15 @@
+public class Even_NotEven {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 10; i++) {
+            if (i % 2 == 0) {
+                System.out.print("Число " + '='+' ' + i  + " Чётное");
+                System.out.println();
+            } else {
+                System.out.print("Число " + '='+' ' + i  + " Не Чётное");
+                System.out.println();
+            }
+        }
+    }
+}
+
+
